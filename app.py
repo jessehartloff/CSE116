a2232fb9c8c98c99eb878a0856169b055622d2e2
jessehartloff <hartloff@buffalo.edu>
2020-01-25 13:00:32 -0500

    Preparing for Spring 2020

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ def cse116():
     return render_template('CSE116.html')
 
 
+@app.route('/cse116/f19')
+def cse116_f19():
+    return render_template('f19_CSE116.html')
+
+
 @app.route('/cse116/s19')
 def cse116_s19():
-    return render_template('old_CSE116.html')
+    return render_template('s19_CSE116.html')
 
 
 @app.route('/static_files/<path:filename>')
