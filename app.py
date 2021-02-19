0920b64964950973b18bf6d399672eed92f78259
jessehartloff <hartloff@buffalo.edu>
2021-02-19 07:34:53 -0500

    Add nosniff

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, make_response
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def cse116():
-    return render_template('CSE116.html')
+    resp = make_response(render_template('CSE116.html'))
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    return resp
 
 
 @app.route('/f19')
 def cse116_f19():
-    return render_template('CSE116.html')
+    resp = make_response(render_template('CSE116.html'))
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    return resp
 
 
 @app.route('/s19')
 def cse116_s19():
-    return render_template('CSE116.html')
+    resp = make_response(render_template('CSE116.html'))
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    return resp
 
 
 @app.route('/static_files/<path:filename>')
 def send_style(filename):
-    return send_from_directory('static_files', filename)
+    resp = make_response(send_from_directory('static_files', filename))
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    return resp
 
 
 if __name__ == '__main__':
